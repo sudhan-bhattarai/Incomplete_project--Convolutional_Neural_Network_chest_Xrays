28482771679d49c5de6438753169cc616258ce4f
sudhan-bhattarai <sudhan.bhattarai26@gmail.com>
2020-12-21 18:42:27 -0700

    update

--- disease-VS-no_disease.py
+++ disease-VS-no_disease.py
@@ -1,5 +1,35 @@
+# %%
+'''Libraries'''
+
+import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
-df = pd.DataFrame(pd.read_csv("Data_Entry_2017_v2020.csv"))
+
+# %%
+'''Load data'''
+
+df = pd.read_csv(r'Data_Entry_2017_v2020.csv')
+df.head()
+
+# %%
+
+# %%
+'''Load images'''
+
+images = []
+path = 'C:\\Users\\stan\\chest_images\\'
+dir = os.listdir(path)
+for image in dir:
+    im_dir = path + str(image)
+    images.append(tf.keras.preprocessing.image.load_img(im_dir))
+
+# %%
+
+'''Image preprocessing'''
+
+
+
+# %%
